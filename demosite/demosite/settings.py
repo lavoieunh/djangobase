531b0179ea3da763efa6477d0878cc564e36e5ee
Ken Lavoie <ken.lavoie@docker.com>
2017-08-11 15:43:57 -0400

    Update settings.py

--- demosite/demosite/settings.py
+++ demosite/demosite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'docker_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
