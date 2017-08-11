f04f1615b41b4d7b69bf5ee881665a2adfacc48e
Ken Lavoie <ken.lavoie@docker.com>
2017-08-11 15:15:00 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/ash
 
 pip install -U pip \
-&& pip install psycopg2 \
-&& pip install -r requirements.txt \ 
-&& pip install --upgrade wheel \
-&& pip install --upgrade setuptools 
+&& pip install -r requirements.txt \
+&& pip install psycopg2
