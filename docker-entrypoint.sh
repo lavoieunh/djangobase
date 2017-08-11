25fae30adacd054d7c00be82b1caf4f1a51daa2c
Ken Lavoie <ken.lavoie@docker.com>
2017-08-11 14:41:35 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,7 @@
-apk add python3-dev \ 
-&& postgresql-dev \ 
-&& gcc 
+#!/bin/ash
 
 pip install -U pip \
-  && pip install -r requirements.txt \ 
-  && pip install --upgrade wheel \
-  && pip install --upgrade setuptools \
-  && pip install psycopg2
+&& pip install psycopg2 \
+&& pip install -r requirements.txt \ 
+&& pip install --upgrade wheel \
+&& pip install --upgrade setuptools 
